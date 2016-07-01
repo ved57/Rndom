7d7c965aade81f2e4850132812484afe3df02c42
root <root@localhost.localdomain>
2016-07-01 14:51:35 -0400

    Implemented multithreaded UDP packet application

--- Producer_Consumer_app.c
+++ Producer_Consumer_app.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <pthread.h>
-#include  <stdlib.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <netdb.h>
@@ -10,172 +10,153 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-
-
 # define BUFFER_SIZE 2
 # define MESSAGE_SIZE 20
 
-void *Producer();
-void *Consumer();
+void *Enqueuer();
+void *Dequeuer();
 
-int BufferIndex=0;
-char BUFFER[BUFFER_SIZE][MESSAGE_SIZE];
-char *Msg1 = "Hello";
-char *Msg2 = "Hai";
+unsigned int readIndex = 0;
+unsigned int writeIndex = 0;
+char dataBuffer[BUFFER_SIZE][MESSAGE_SIZE];
 
-pthread_cond_t Buffer_Not_Full=PTHREAD_COND_INITIALIZER;
-pthread_cond_t Buffer_Not_Empty=PTHREAD_COND_INITIALIZER;
-pthread_mutex_t mVar=PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t bufferNotFull=PTHREAD_COND_INITIALIZER;
+pthread_cond_t bufferNotEmpty=PTHREAD_COND_INITIALIZER;
+pthread_mutex_t mutexLock=PTHREAD_MUTEX_INITIALIZER;
 
-int producer_main()
-{    
-    pthread_t ptid,ctid;
-    
-    //BUFFER=(char *) malloc(sizeof(char) * BUFFER_SIZE);            
-    
-    pthread_create(&ptid,NULL,Producer,NULL);
-    pthread_create(&ctid,NULL,Consumer,NULL);
-    
-    pthread_join(ptid,NULL);
-    pthread_join(ctid,NULL);
-        
-    
-    return 0;
-}
+int sockfd; /* socket */
+int portno; /* port to listen on */
+int clientlen; /* byte size of client's address */
+struct sockaddr_in serveraddr; /* server's addr */
+struct sockaddr_in clientaddr; /* client addr */
+struct hostent *hostp; /* client host info */	
+char *hostaddrp; /* dotted decimal host addr string */
+int optval; /* flag value for setsockopt */
+int n; /* message byte size */
 
 
 /*
  * error - wrapper for perror
  */
-void error(char *msg) {
-	perror(msg);
-	exit(1);
+ void error(char *msg) {
+ 	perror(msg);
+ 	exit(1);
+ }
+
+
+ void *Enqueuer()
+ {    
+ 	while(1)
+ 	{
+ 		printf("Enqueuer entered \n");
+ 		pthread_mutex_lock(&mutexLock);
+ 		printf("Enqueuer acquired lock\n");
+ 		if(abs(writeIndex - readIndex) == BUFFER_SIZE)
+ 		{
+ 			printf("Waiting for buffer not to be full\n");
+ 			writeIndex %= BUFFER_SIZE;
+ 			pthread_cond_wait(&bufferNotFull,&mutexLock);
+ 		}
+
+        /*
+		 * recvfrom: receive a UDP datagram from a client
+		 */
+		 memset(dataBuffer[writeIndex],0,MESSAGE_SIZE);
+		 n = recvfrom(sockfd, dataBuffer[writeIndex], MESSAGE_SIZE, 0,
+		 	(struct sockaddr *) &clientaddr, &clientlen);
+		 if (n < 0)
+		 	error("ERROR in recvfrom");
+
+		 printf("Received datagram \t %s \n", dataBuffer[writeIndex]);
+		 
+		 printf("Written index : %u \n",writeIndex);        
+		 writeIndex = (writeIndex + 1);
+		 pthread_mutex_unlock(&mutexLock);
+		 printf("Enqueuer released teh lock \n");
+		 pthread_cond_signal(&bufferNotEmpty);        
+		}        
+	}
+
+	void *Dequeuer()
+	{
+		while(1)
+		{
+			printf("\n Dequeuer entered");
+			pthread_mutex_lock(&mutexLock);
+			printf("\n Dequeuer acquired lock");
+			if(readIndex == writeIndex)
+			{            
+				pthread_cond_wait(&bufferNotEmpty,&mutexLock);
+			}
+
+	 /*
+	 * sendto: echo the input back to the client
+	 */
+	 printf("Dequeuer::Sendto is going to be called. \n");
+	 n = sendto(sockfd, dataBuffer, strlen(dataBuffer[readIndex]), 0,
+	 	(struct sockaddr *) &clientaddr, clientlen);
+	 printf("\n Send to result is %d ", n);
+
+	 if (n < 0)
+	 	error("ERROR in sendto");
+
+	 printf("Consume : %d \n",readIndex);        
+	 readIndex = (readIndex + 1) % BUFFER_SIZE;
+	 pthread_mutex_unlock(&mutexLock);        
+	 pthread_cond_signal(&bufferNotFull);                
+	}
 }
 
+	
 int main(int argc, char **argv) {
-	int sockfd; /* socket */
-	int portno; /* port to listen on */
-	int clientlen; /* byte size of client's address */
-	struct sockaddr_in serveraddr; /* server's addr */
-	struct sockaddr_in clientaddr; /* client addr */
-	struct hostent *hostp; /* client host info */
-	char buf[BUFSIZE]; /* message buf */
-	char *hostaddrp; /* dotted decimal host addr string */
-	int optval; /* flag value for setsockopt */
-	int n; /* message byte size */
-
 	/*
 	 * check command line arguments
 	 */
-	if (argc != 2) {
-		fprintf(stderr, "usage: %s <port>\n", argv[0]);
-		exit(1);
-	}
-	portno = atoi(argv[1]);
+	 if (argc != 2) {
+	 	fprintf(stderr, "usage: %s <port>\n", argv[0]);
+	 	exit(1);
+	 }
+	 portno = atoi(argv[1]);
 
 	/*
 	 * socket: create the parent socket
 	 */
-	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-	if (sockfd < 0)
-		error("ERROR opening socket");
+	 sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	 if (sockfd < 0)
+	 	error("ERROR opening socket");
 
-	optval = 1;
-	setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR,
-			(const void *)&optval , sizeof(int));
+	 optval = 1;
+	 setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR,
+	 	(const void *)&optval , sizeof(int));
 
 	/*
 	 * build the server's Internet address
 	 */
-	bzero((char *) &serveraddr, sizeof(serveraddr));
-	serveraddr.sin_family = AF_INET;
-	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	serveraddr.sin_port = htons((unsigned short)portno);
+	 bzero((char *) &serveraddr, sizeof(serveraddr));
+	 serveraddr.sin_family = AF_INET;
+	 serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	 serveraddr.sin_port = htons((unsigned short)portno);
 
 	/*
 	 * bind: associate the parent socket with a port
 	 */
-	if (bind(sockfd, (struct sockaddr *) &serveraddr,
-			sizeof(serveraddr)) < 0)
-		error("ERROR on binding");
+	 if (bind(sockfd, (struct sockaddr *) &serveraddr,
+	 	sizeof(serveraddr)) < 0)
+	 	error("ERROR on binding");
 
 	/*
 	 * main loop: wait for a datagram, then echo it
 	 */
-	clientlen = sizeof(clientaddr);
-	int bReceivedFirstPacket = 0;
-	struct timespec start, stop;
-	while (1) {
-		/*
-		 * recvfrom: receive a UDP datagram from a client
-		 */
-		bzero(buf, BUFSIZE);
-		n = recvfrom(sockfd, buf, BUFSIZE, 0,
-				(struct sockaddr *) &clientaddr, &clientlen);
-		if (n < 0)
-			error("ERROR in recvfrom");
+	 clientlen = sizeof(clientaddr);	
 
-		/*
-		 * gethostbyaddr: determine who sent the datagram
-		 */
-		hostp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr,
-				sizeof(clientaddr.sin_addr.s_addr), AF_INET);
-		if (hostp == NULL)
-			error("ERROR on gethostbyaddr");
-		hostaddrp = inet_ntoa(clientaddr.sin_addr);
-		if (hostaddrp == NULL)
-			error("ERROR on inet_ntoa\n");
-		
-		printf("server received datagram from %s \t (%s)  \t %s \n",
-			hostp->h_name, hostaddrp, buf);
-		/*
-		 * sendto: echo the input back to the client
-		 */
-		n = sendto(sockfd, buf, strlen(buf), 0,
-				(struct sockaddr *) &clientaddr, clientlen);
-		if (n < 0)
-			error("ERROR in sendto");
+
+	 pthread_t ptid,ctid;    
+
+	 pthread_create(&ptid,NULL,Enqueuer,NULL);
+	 pthread_create(&ctid,NULL,Dequeuer,NULL);
+
+	 pthread_join(ptid,NULL);
+	 pthread_join(ctid,NULL);
+
+	 return 0;
 	}
-
-}
-
-
-
-
-
-
-
-
-
-
-void *Producer()
-{    
-    for(;;)
-    {
-        pthread_mutex_lock(&mVar);
-        if(BufferIndex==BUFFER_SIZE)
-        {                        
-            pthread_cond_wait(&Buffer_Not_Full,&mVar);
-        }                        
-        BUFFER[BufferIndex++]='@';
-        printf("Produce : %d \n",BufferIndex);
-        pthread_mutex_unlock(&mVar);
-        pthread_cond_signal(&Buffer_Not_Empty);        
-    }    
-    
-}
-
-void *Consumer()
-{
-    for(;;)
-    {
-        pthread_mutex_lock(&mVar);
-        if(BufferIndex==-1)
-        {            
-            pthread_cond_wait(&Buffer_Not_Empty,&mVar);
-        }                
-        printf("Consume : %d \n",BufferIndex--);        
-        pthread_mutex_unlock(&mVar);        
-        pthread_cond_signal(&Buffer_Not_Full);                
-    }    
-}
